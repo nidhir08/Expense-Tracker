94426531a88387aaa2cf0e2e608835815f4ba5fc
Nidhi Rawat <168139652+nidhir08@users.noreply.github.com>
2025-05-14 02:37:42 +0530

    header and routing

---
+++ src/app/header.tsx
@@ -0,0 +1,58 @@
+'use client'
+import React from 'react'
+import {Settings, Bell} from 'lucide-react'
+import Image from "next/image" 
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+const Header = () => {
+  const pathname = usePathname()
+  const menuItems = [
+    { name: 'Dashboard', href: '/' },
+    { name: 'Transactions', href: '/transactions' },
+    { name: 'Analytics', href: '/analytics' },
+    { name: 'Savings', href: '/savings' },
+    { name: 'Accounts', href: '/accounts' },
+  ]
+  return (
+    <div className='flex flex-row bg-white  text-black gap-90  '>
+        <div className="relative w-60 h-15">
+  <Image
+    src="/images/Trackwise.jpg"
+    alt="logo"
+    fill
+    className="object-cover"
+  />
+</div>
+
+        <div className='flex flex-row gap-4 pt-5'>
+        {menuItems.map((item) => (
+          <Link
+        key={item.name}
+        href={item.href}
+        className={`px-4 py-2 rounded cursor-pointer font-medium ${
+          pathname === item.href
+            ? 'bg-blue-500 text-white'
+            : 'bg-gray-100 text-black'
+        }`}
+      >
+        {item.name}
+      </Link>
+      ))}
+        </div>
+     <div className='flex flex-row pt-5 gap-4'>
+     <Settings />
+     <Bell />
+     <div className='relative w-10 h-10 md:w-20 md:h-10 rounded-full overflow-hidden'>
+      <Image
+        src='https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww'
+        alt='William Cameron'
+        fill
+        className='object-cover'
+      />
+    </div>
+     </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/app/transactions/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../header'
+
+const Transactions = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default Transactions
